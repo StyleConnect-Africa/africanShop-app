63cfed9e238d1969f70364c40ee05cdca7576344
Henry Amos <amoshenry95@gmail.com>
2024-11-24 21:40:33 +0000

    📦 build: build files for production 🚀 build(production): generate production files 🔨 build: create production build

--- src/pages/dashboard/components/ProfileSection.jsx
+++ src/pages/dashboard/components/ProfileSection.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import  { useState, useEffect } from "react";
 import Avatar from "react-avatar-edit";
 import { toast } from "react-toastify";
 import Modal from "react-modal";
